cf4cd69100202f03c0c5ac7225b845c81b2f16d2
FranzBogmann <franz.bogmann@web.de>
2020-07-07 20:12:11 +0200

    Create Bewegung.java

---
+++ Erster-Bot/src/Bewegung.java
@@ -0,0 +1,18 @@
+/**
+ * @author Franz
+ *
+ * Diese Klasse, soll die Bewegung mittels system.out.println() in der main Methode der Klasse ErsterBot/Team11Bot ausfuehren, 
+ * indem die Methode Bewegung.bewegung() dort aufgerufen wird, welche einen String (Aktion) zurueckgibt.
+ * 
+ */
+public class Bewegung {
+	
+	//Vor der Bewegung
+	
+	
+	
+	
+	
+	
+
+}
