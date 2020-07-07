06c65471cda3c72c5d1f97fa6a509b4733f49206
Alexander Schmidt <67632384+alexanderschmidt2@users.noreply.github.com>
2020-07-07 21:39:24 +0200

    Revert "Revert "Merge branch 'Franz+Alex_development' of https://github.com/alexanderschmidt2/Coding-Challenge-Team-11 into Franz+Alex_development""
    This reverts commit 92ebba6736d2d14a67cb237ed3d59be772cf3166.

--- Erster-Bot/src/Bewegung.java
+++ Erster-Bot/src/Bewegung.java
@@ -1,4 +1,23 @@
+<<<<<<< HEAD
 
 public class Bewegung {
+=======
+/**
+ * @author Franz
+ *
+ * Diese Klasse, soll die Bewegung mittels system.out.println() in der main Methode der Klasse ErsterBot/Team11Bot ausfuehren, 
+ * indem die Methode Bewegung.bewegung() dort aufgerufen wird, welche einen String (Aktion) zurueckgibt.
+ * 
+ */
+public class Bewegung {
+	
+	//Vor der Bewegung
+	
+	
+	
+	
+	
+	
+>>>>>>> cf4cd69100202f03c0c5ac7225b845c81b2f16d2
 
 }
