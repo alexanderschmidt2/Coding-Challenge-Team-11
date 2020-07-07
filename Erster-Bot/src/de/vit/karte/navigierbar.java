3961403c5629287d2885129c40697d1c65cff2fc
Alexander Schmidt <67632384+alexanderschmidt2@users.noreply.github.com>
2020-07-07 22:36:05 +0200

    Kleinere Anpassungen
    In navigierbar die benötigten Methoden reingehauen. FIXME: eine Sprache, eine Nomenklatur v. Methodennamen und Parametern. Also entweder voll deutsch oder voll englisch.
    Was die Position angeht so wurde da gute Arbeit geleistet. Über die Methoden müssen wir uns jedoch unterhalten. Ich denke diese koordinatenberechnung ist halt auch von relativen Koordinaten abhängig, die wir tracken. Also da steige ich noch nicht ganz durch die Logik durch.
    Euer Map Array ist eindimensional.

--- Erster-Bot/src/de/vit/karte/navigierbar.java
+++ Erster-Bot/src/de/vit/karte/navigierbar.java
@@ -1,5 +1,10 @@
 package de.vit.karte;
 
 public interface navigierbar {
-
+	public void setzeNeuesKartenfeld();
+	public int getEntfernung();
+	public int setEntfernung();
+	public Koordinaten getStart();
+	public int setKartenfeld(Koordinaten koordinaten);
+	public int getKartenfeld(Koordinaten koordinaten);
 }
