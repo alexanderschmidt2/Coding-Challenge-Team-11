0a08385e076861172d98f38fb4c63e1f2da9c7c2
Alexander Schmidt <67632384+alexanderschmidt2@users.noreply.github.com>
2020-07-09 19:19:18 +0200

    Revert "Test"
    This reverts commit 780c364a38fb3b0a871bba42e421cfa6f58773e3.

--- Erster-Bot/src/de/vit/karte/felder/Sachbearbeiter.java
+++ Erster-Bot/src/de/vit/karte/felder/Sachbearbeiter.java
@@ -4,15 +4,10 @@ public class Sachbearbeiter extends Feld {
 
 	private final int playerId;
 	private final int formCount;
-<<<<<<< HEAD
 	private boolean unserSB;
 
 	//Setter werden nicht benötigt, da beim Konstruktor Aufruf die playerId und der formCount unveränderbar sind.
 
-=======
-	
-	//Getter und Setter
->>>>>>> parent of 2b35463... Update und Methode aktualisereNorden angefangen
 	public int getPlayerId() {
 		return playerId;
 	}
@@ -20,14 +15,8 @@ public class Sachbearbeiter extends Feld {
 	public int getFormCount() {
 		return formCount;
 	}
-<<<<<<< HEAD
 
 	public Sachbearbeiter(int playerId, int formCount) {
-=======
-	
-	public Sachbearbeiter(int playerId, int formCount)
-	{
->>>>>>> parent of 2b35463... Update und Methode aktualisereNorden angefangen
 		super();
 		this.playerId = playerId;
 		this.formCount = formCount;
