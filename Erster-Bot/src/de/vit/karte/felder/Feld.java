3332d4676850d23f71eb67be4480702a14b7869f
Alexander Schmidt <67632384+alexanderschmidt2@users.noreply.github.com>
2020-07-09 18:15:34 +0200

    Revert "Merge branch 'Laura+Constantin'"
    This reverts commit fdda75b043cd9e5bfb6b6543fcdf8863ce6e056e, reversing
    changes made to 910490c171d6ac653b716b3eff0808b291ab2ab7.

--- Erster-Bot/src/de/vit/karte/felder/Feld.java
+++ Erster-Bot/src/de/vit/karte/felder/Feld.java
@@ -1,59 +1,48 @@
 package de.vit.karte.felder;
 
 import de.vit.karte.*;
-
 /**
  * 
  * @author Laura
  *der Name der jeweiligen Spezialisierung entspricht der Bezeichnung der Spieleumgebung für das Feld
  */
 public abstract class Feld {
-	/**
-	 * protected, da erst in der Unterklasse initialisiert
-	 */
+	//protected, da erst in der Unterklasse initialisiert
 	protected String name;
-	private int entfernung;
+	protected String typ;
+	//wie oft bin ich bereits über dieses Feld gelaufen?
 	private int besuche = 0;
-	
-	//Getter und Setter
+	private int entfernung = 500000000;
+
 	/**
-	 * kein Setter, da Name nicht geändert werden soll, bei Änderung der Feldart wird ein neues Objekt einer anderen Klasse angelegt
+	 * kein Setter, da Name nicht geändert werden soll
 	 * @return
 	 */
 	public String getName() {
 		return name;
 	}
-
-	public int getEntfernung() {
-		return entfernung;
-	}
-
-	public void setEntfernung(int entfernung) {
-		this.entfernung = entfernung;
-	}
+	public String getTyp() {
+		return typ;
+	};
 
 	public int getBesuche() {
 		return besuche;
 	}
-
 	public void setBesuche(int besuche) {
 		this.besuche = besuche;
 	}
-
+	public int getEntfernung() {
+		return entfernung;
+	}
+	public void setEntfernung(int entfernung) {
+		this.entfernung = entfernung;
+	}
 	/**
 	 * die Koordinate wird mithilfe des Setters gesetzt, da beim Sachbearbeiter die Koordinate zunächst nicht bekannt ist
 	 */
 	public Feld()
 	{
-		
-	}
-		
-	//probe-Main, wird noch gelöscht...
-	public static void main (String[] args)
-	{
-		Feld[] test = new Feld[2];
-		test[0] = new Boden();
-		System.out.println(test[0].getName());
+		this.position = new Koordinate();
 	}
 
 }
