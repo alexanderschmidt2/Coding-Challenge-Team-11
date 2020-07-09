780c364a38fb3b0a871bba42e421cfa6f58773e3
Alexander Schmidt <67632384+alexanderschmidt2@users.noreply.github.com>
2020-07-09 19:16:58 +0200

    Test

--- Erster-Bot/src/de/vit/karte/Karte.java
+++ Erster-Bot/src/de/vit/karte/Karte.java
@@ -14,11 +14,28 @@ public class Karte implements navigierbar {
 	private Feld[][] aktuelleKarte;
 	// das eigentliche Spielfeld mit allen Feldern
 	private final int level;
+<<<<<<< HEAD
 	private final int playerId;
 	private int[] aktuellePosition;
 
 	public int getSizeX() {
 		return sizeX;
+=======
+	/**
+	 * das eigentliche Spielfeld mit allen Feldern
+	 * die erste Array-Ebene bezeichnet die x-Achse
+	 * die zweite Array-Ebene bezeichnet die y-Achse
+	 */
+	private Feld[] [] karte;
+	//die momentane Position, wird regelmäßig aktualisiert
+	private int [] aktuellePosition = new int [2];
+	
+	
+	//Getter und Setter
+	public int[] getSize() {
+		int [] size = new int[] {sizeX, sizeY};
+		return size;
+>>>>>>> parent of 2b35463... Update und Methode aktualisereNorden angefangen
 	}
 
 	public int getSizeY() {
@@ -36,10 +53,13 @@ public class Karte implements navigierbar {
 	public int getLevel() {
 		return level;
 	}
+<<<<<<< HEAD
 
 	public int getPlayerId() {
 		return playerId;
 	}
+=======
+>>>>>>> parent of 2b35463... Update und Methode aktualisereNorden angefangen
 
 	public void setAktuellePosition(int x, int y) {
 		this.aktuellePosition[0] = x;
@@ -58,14 +78,24 @@ public class Karte implements navigierbar {
 	 * @param startX Startposition des Bots auf der x-Achse
 	 * @param startY Startposition des Bots auf der y-Achse
 	 */
+<<<<<<< HEAD
 	public Karte(int sizeX, int sizeY, int level, int playerId, int startX, int startY) {
+=======
+	public Karte(int sizeX, int sizeY, int level, int startX, int startY)
+	{
+>>>>>>> parent of 2b35463... Update und Methode aktualisereNorden angefangen
 		this.sizeX = sizeX;
 		this.sizeY = sizeY;
 		this.level = level;
+<<<<<<< HEAD
 		this.playerId = playerId;
 		this.setAktuellePosition(startX, startY);
 		karteGenerieren();
 
+=======
+		this.aktuellePosition[0] = startX;
+		this.aktuellePosition[1] = startY;
+>>>>>>> parent of 2b35463... Update und Methode aktualisereNorden angefangen
 	}
 
 	public void karteGenerieren() {
@@ -133,5 +163,78 @@ public class Karte implements navigierbar {
 			break;
 		}
 	}
+<<<<<<< HEAD
+=======
+	
+	public void aktualisiereKarte(Rundeninformationen rundeninformation)
+	{
+		//TODO: maximal 3 unbekannte neue Felder durch neue Informationen ersetzen
+	}
+	
+	/**
+	 * Methode, die abhängig vom aktuellen Standpunkt die Koordinaten des im Norden angrenzenden Objekts zurückgibt
+	 * @return  Koordinaten des Objekts nördlich vom aktuellen Standpunkt, welches eine Spezialisierung des Typs Feld ist
+	 */
+	public int[] getNorden(int x, int y)
+	{
+		int[] nord = new int[2];
+		nord[0] = x;
+		nord[1] = ((y - 1) + this.getSize()[1]) % this.getSize()[1];
+		return nord;
+	}
+	
+	/**
+	 * s. Norden, nur mit Osten...
+	 * @return
+	 */
+	public int[] getOsten(int x, int y)
+	{
+		int[] ost = new int[2];
+		ost[0] = ((x + 1) + this.getSize()[0]) % this.getSize()[0];
+		ost[1] = y;
+		return ost;
+	}
+	
+	/**
+	 * s. Norden, nur mit Süden...
+	 * @return
+	 */
+	public int[] getSueden(int x, int y)
+	{
+		int[] sued = new int[2];
+		sued[0] = x;
+		sued[1] = ((y + 1) + this.getSize()[1]) % this.getSize()[1];
+		return sued;
+	}
+	
+	/**
+	 * s. Norden, nur mit Westen...
+	 * @return
+	 */
+	public int[] getWesten(int x, int y)
+	{
+		int[] west = new int[2];
+		west[0] = ((x - 1) + this.getSize()[0]) % this.getSize()[0];
+		west[1] = y;
+		return west;
+	}
+	
+	/**
+	 * Methode, die ein Array von vier Feldern zurückgibt, welche den Feldern entsprechen, die an die aktuelle Position angrenzen
+	 * die Reihenfolge der Objekte: Nord, Ost, Süd, West
+	 * die Methode ruft die Methoden getNorden(), getOsten(), getSüden(), getWesten() auf
+	 * @param karte
+	 * @return
+	 */
+	public Feld[] getNachbarn(int x, int y)
+	{
+		Feld[] nachbarn = new Feld[4];
+		nachbarn[0] = getFeld(this.getNorden(x, y)[0], this.getNorden(x, y)[1]);
+		nachbarn[1] = getFeld(this.getOsten(x, y)[0], this.getOsten(x, y)[1]);
+		nachbarn[2] = getFeld(this.getSueden(x, y)[0], this.getSueden(x, y)[1]);
+		nachbarn[3] = getFeld(this.getWesten(x, y)[0], this.getWesten(x, y)[1]);
+		return nachbarn;
+	}
+>>>>>>> parent of 2b35463... Update und Methode aktualisereNorden angefangen
 
 }
--- Erster-Bot/src/de/vit/karte/felder/Sachbearbeiter.java
+++ Erster-Bot/src/de/vit/karte/felder/Sachbearbeiter.java
@@ -4,10 +4,15 @@ public class Sachbearbeiter extends Feld {
 
 	private final int playerId;
 	private final int formCount;
+<<<<<<< HEAD
 	private boolean unserSB;
 
 	//Setter werden nicht benötigt, da beim Konstruktor Aufruf die playerId und der formCount unveränderbar sind.
 
+=======
+	
+	//Getter und Setter
+>>>>>>> parent of 2b35463... Update und Methode aktualisereNorden angefangen
 	public int getPlayerId() {
 		return playerId;
 	}
@@ -15,8 +20,14 @@ public class Sachbearbeiter extends Feld {
 	public int getFormCount() {
 		return formCount;
 	}
+<<<<<<< HEAD
 
 	public Sachbearbeiter(int playerId, int formCount) {
+=======
+	
+	public Sachbearbeiter(int playerId, int formCount)
+	{
+>>>>>>> parent of 2b35463... Update und Methode aktualisereNorden angefangen
 		super();
 		this.playerId = playerId;
 		this.formCount = formCount;
