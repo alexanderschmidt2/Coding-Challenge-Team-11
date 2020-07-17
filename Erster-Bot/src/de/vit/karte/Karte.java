d66d83f89b53d087ce36d169905378c09c121396
Laura <Laura@Laura-PC>
2020-07-17 20:29:14 +0200

    vernebleKarte() fertig (noch ungetestet und verbesserungswürdig)

--- Erster-Bot/src/de/vit/karte/Karte.java
+++ Erster-Bot/src/de/vit/karte/Karte.java
@@ -764,12 +764,16 @@ public class Karte implements navigierbar {
 		{
 			for (int j = 0; j < size[0]; j++)
 			{
-				if (this.karte[j] [i].getEntfernung() <= 3)
+				if ((this.karte[j] [i].getEntfernung() == 2 || this.karte[j] [i].getEntfernung() == 4) && !(this.karte[j] [i] instanceof Dokument) && !(this.karte[j] [i] instanceof Sachbearbeiter) &&
+						((this.getNachbarn(new int [] {j,i})[0].getEntfernung() == 500000000 && this.getNachbarn(new int [] {j,i})[2].getEntfernung() == 500000000) ||
+						(this.getNachbarn(new int [] {j,i})[1].getEntfernung() == 500000000 && this.getNachbarn(new int [] {j,i})[3].getEntfernung() == 500000000)))
 				{
-					this.karte[j] [i] = new Nebel();
+						this.karte[j] [i] = new Nebel();
+						
 				}
 			}
 		}
+		this.aktualisiereEntfernung();
 	}
 
 	/**
