acdb1e49da98d0579a1c77bd4268bcb8ceba0f67
cgX42 <67632227+cgX42@users.noreply.github.com>
2020-07-20 19:53:35 +0200

    Karte verfeinert, Bewegung auch

--- Erster-Bot/src/de/vit/karte/Inavigierbar.java
+++ Erster-Bot/src/de/vit/karte/Inavigierbar.java
@@ -5,22 +5,19 @@ import de.vit.karte.typen.ZielMap;
 
 public interface Inavigierbar {
 
-	public int getLevel();
-	public int[] getAktuellePosition() ;
-	public Feld getFeld(int[] koordinaten);
-	public int getFormCount();
-	public int getSheetCount();
-	public ZielMap getStatischeZiele();
-	public void reduziereSheetCount();
-	public void vernebleKarte();
-	public Feld[] getNachbarn(int[] koordinaten);
-	public int[] getDynamischesZiel();
-	public void vernebleUmgebung();
-	public int[] getNorden(int[] aktuelleKoordinaten);
-	public int[] getOsten(int[] aktuelleKoordinaten);
-	public int[] getSueden(int[] aktuelleKoordinaten);
-	public int[] getWesten(int[] aktuelleKoordinaten);
-
-	
-
+        public int getLevel();
+        public int[] getAktuellePosition() ;
+        public Feld getFeld(int[] koordinaten);
+        public int getFormularZaehler();
+        public int getBlattZaehler();
+        public ZielMap getStatischeZiele();
+        public void reduziereBlattZaehler();
+        public void vernebleKarte();
+        public Feld[] getNachbarn(int[] koordinaten);
+        public int[] getDynamischesZiel();
+        public void vernebleUmgebung();
+        public int[] getNorden(int[] aktuelleKoordinaten);
+        public int[] getOsten(int[] aktuelleKoordinaten);
+        public int[] getSueden(int[] aktuelleKoordinaten);
+        public int[] getWesten(int[] aktuelleKoordinaten);
 }
